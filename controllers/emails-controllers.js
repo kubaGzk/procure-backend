3ea925dc423c7d1a21389fd9f24ae294e51857e3
Kuba <jacob_5@op.pl>
2020-09-09 18:17:35 +0200

    email works

--- controllers/emails-controllers.js
+++ controllers/emails-controllers.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 const pdfGenerator = require("./pdf-controllers");
 const fs = require("fs");
 
@@ -6,12 +8,31 @@ const Supplier = require("../models/supplier");
 
 const sendOrder = (orderData) =>
   new Promise(async (resolve, reject) => {
+    const oauth2Client = new OAuth2(
+      process.env.OAUTH2_CLIENT,
+      process.env.OAUTH2_CLIENT_SEC,
+      process.env.OAUTH2_REDIRECT_URL
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.OAUTH2_REF_TOKEN
+    })
+
+    const accessToken = await oauth2Client.getAccessToken();
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
+        type: "OAuth2",
         user: "procurepurchaseorders@gmail.com",
-        pass: "Procure1Admin2",
-      },
+        clientId: process.env.OAUTH2_CLIENT,
+        clientSecret: process.env.OAUTH2_CLIENT_SEC,
+        refreshToken: process.env.OAUTH2_REF_TOKEN,
+        accessToken: accessToken
+        
+      },tls: {
+        rejectUnauthorized: false
+      }
     });
     const supplier = await Supplier.findById(orderData.supplier.id);
     if (!supplier) {
@@ -21,7 +42,7 @@ const sendOrder = (orderData) =>
     let htmlStream;
     try {
       orderPath = pdfGenerator.generateOrder(orderData);
-      htmlStream = fs.createReadStream('./email/email.html');
+      htmlStream = fs.createReadStream("./email/email.html");
     } catch (err) {
       console.log(err);
     }
@@ -29,7 +50,8 @@ const sendOrder = (orderData) =>
       from: "procurepurchaseorder@gmail.com",
       to: supplier.contact.email,
       subject: `Purchase order ${orderData.orderId}`,
-      text: "Your partner is sending you new purchase order. PDF copy attached.",
+      text:
+        "Your partner is sending you new purchase order. PDF copy attached.",
       html: htmlStream,
       attachments: [
         {
